b19e1b8d197092d7e235b322d2acf642311a3121
widres8 <williandres8@gmail.com>
2019-07-18 18:07:12 -0500

    add modules for folders

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 import { ComponentsModule } from './components/components.module';
+import { ServicesIndexModule } from './services/services-index.module';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,7 @@ import { ComponentsModule } from './components/components.module';
     BrowserAnimationsModule,
     MaterialModule,
     ComponentsModule,
+    ServicesIndexModule,
   ],
   providers: [
 
---
+++ frontend/src/app/config/config.ts
@@ -0,0 +1,4 @@
+
+export const config = {
+  URL_SERVICES_API: 'http://localhost:8000/api',
+};
