8a0097789692ade9482dc19a776f4c160cfa9ed7
widres8 <williandres8@gmail.com>
2019-07-20 15:34:23 -0500

    update project

--- frontend/src/app/config/config.ts
+++
@@ -1,4 +0,0 @@
-
-export const config = {
-  URL_SERVICES_API: 'http://localhost:8000/api',
-};
--- frontend/src/app/layout/fixedplugin/setting-theme.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface SettingTheme {
-  sidebarColor: string;
-  sidebarActiveColor: string;
-}
